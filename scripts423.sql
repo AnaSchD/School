a7a3e9364302ab28f3d54355e3b7ab45e0f40c10
Scherbakova <nastyasch0806@gmail.com>
2023-03-03 16:52:22 +0300

    first commit управление схемами базы данных

---
+++ scripts423.sql
@@ -0,0 +1,2 @@
+SELECT student.name, student.age, faculty.name FROM student INNER JOIN faculty ON student.faculty_id = faculty.id;
+SELECT student.name FROM student INNER JOIN avatar a ON student.id = a.student_id;
